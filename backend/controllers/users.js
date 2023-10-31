2a666aae0b2550e2f493a8fa8e9eb306685338c6
Elisaveta Vlad <vladelisabeta@yandex.ru>
2023-10-31 19:14:55 +0300

    удалена папка ноды

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -122,9 +122,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
 
-      console.log('ХОТЕЛ ТОКЕН ДА?? А ИЗ ЛОГИНА ОН НЕ ИДЕТ');
       res.send({ token });
-      console.log({ token }, 'токен в логине на отправку');
       // res.send(token);
     })
     .catch(next);
