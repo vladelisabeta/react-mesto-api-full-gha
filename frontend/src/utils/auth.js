04c3fd4371b40e14f6018eacdaf85b41ec999c2a
Elisaveta Vlad <vladelisabeta@yandex.ru>
2023-10-31 21:56:44 +0300

    окончальеный фикс апи ссылки

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.mesto.vladelisabeta.nomoredomainsrocks.ru/signin'
+export const BASE_URL = 'https://api.mesto.vladelisabeta.nomoredomainsrocks.ru'
 
 export function checkResponse(res) {
     if (res.ok) {
